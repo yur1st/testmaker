e722643c0b8c8c24f726842adb885f691f893386
yur1st <okhvat@gmail.com>
2021-12-06 23:30:56 +0300

    Initial commit

---
+++ src/main/java/com/testmaker/TestmakerApplication.java
@@ -0,0 +1,13 @@
+package com.testmaker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestmakerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestmakerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/testmaker/TestmakerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.testmaker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestmakerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
